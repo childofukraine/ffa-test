ea4ab1370aaa3390450964de171a8aa18846e465
childofukraine <98478383+childofukraine@users.noreply.github.com>
2025-09-04 21:03:06 +0300

    chore: changed controller path

--- src/request/request.controller.ts
+++ src/request/request.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateRequestDto } from 'src/common/dtos/request/create-request.dto';
 import { RequestService } from './request.service';
 
-@Controller('request')
+@Controller('requests')
 export class RequestController {
   constructor(private readonly requestService: RequestService) {}
 
